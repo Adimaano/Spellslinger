d002bfc11750f8181c717c36671d067bcc7d055f
Christian Prior <apfelbox@gmail.com>
2023-03-15 10:46:51 +0100

    minor cleanup in draw script + update readme

--- Assets/Scripts/Draw.cs
+++ Assets/Scripts/Draw.cs
@@ -13,19 +13,18 @@ public class Draw : MonoBehaviour {
     private InputDevice device;
 
     private GameObject drawPoint;
-    private GameObject drawEffect;
 
     public Material drawMaterial;
-    //public Material runeMaterial;
 
-    private Animator animator;
     private LineRenderer lineRenderer;
     private GameObject lineRendererParent;
 
     private bool isDrawing = false;
     private float lineWidth = 0.05f;
 
-    private static int ID = 0;
+    private const int MAX_POINTS = 20;
+    private const string FILEPATH = "Runes.txt";
+    private int numberOfRunesSaved = 0;
 
     void GetDevice() {
         InputDevices.GetDevicesAtXRNode(xrNode, devices);
@@ -45,7 +44,9 @@ public class Draw : MonoBehaviour {
         // For Testing Purposes only RightHandController 
         // TODO: implement for both controllers
         drawPoint = GameObject.Find("DrawPointRight");
-        //drawEffect.SetActive(false);
+
+        // get how many lines are currently in the file
+        numberOfRunesSaved = System.IO.File.ReadAllLines(FILEPATH).Length;
     }
 
     void Update() {
@@ -94,7 +95,6 @@ public class Draw : MonoBehaviour {
             lineRenderer.SetPosition(lineRenderer.positionCount++, drawPoint.transform.position);
 
             GameObject spell = new GameObject();
-            // spell.tag = "Spell Rune";
             spell.transform.position = drawPoint.transform.position;
             spell.transform.parent = lineRendererParent.transform;
             SphereCollider sc = spell.AddComponent(typeof(SphereCollider)) as SphereCollider;
@@ -106,8 +106,7 @@ public class Draw : MonoBehaviour {
 
         isDrawing = true;
 
-        lineRendererParent = new GameObject();
-        //lineRendererParent.tag = "Spell Rune";
+        lineRendererParent = new GameObject("Original Rune " + ++numberOfRunesSaved);
         
         // Add Collider
         SphereCollider sc = lineRendererParent.AddComponent(typeof(SphereCollider)) as SphereCollider;
@@ -129,15 +128,12 @@ public class Draw : MonoBehaviour {
         for (int i = 0; i < lineRenderer.positionCount; i++) {
             globalPoints.Add(lineRenderer.GetPosition(i));
         }
-        Debug.Log(globalPoints.Count + "global Points");
 
         Vector3[] savedPoints = GenerateNormalizedList(globalPoints, lineRendererParent.transform);
 
-        Debug.Log(savedPoints.Length + " normalized points");
 
         // save to file if length condition is met
-        if (savedPoints.Length == 20) {
-            string filepath = "Assets/Ressources/Runes.txt";
+        if (savedPoints.Length == MAX_POINTS) {
             //string shapeName = "hourglass";
             
             //string line = ID + ";" + shapeName + ";";
@@ -147,16 +143,14 @@ public class Draw : MonoBehaviour {
             }
             line += "\n";
 
-            System.IO.File.AppendAllText(filepath, line);
-            ID++;
-
+            System.IO.File.AppendAllText(FILEPATH, line);
         } else {
             Debug.Log("Length Condition not met: " + savedPoints.Length + " Points");
         }
 
         // NOTE: only for debugging to check if the created pointcloud matches the expectations
         // create new gameobject with linerenderer and draw the points in savedPoints
-        GameObject newLineRendererParent = new GameObject();
+        GameObject newLineRendererParent = new GameObject("Normalized Rune " + numberOfRunesSaved);
         LineRenderer newLineRenderer = newLineRendererParent.AddComponent<LineRenderer>();
         newLineRenderer.material = drawMaterial;
         newLineRenderer.startWidth = 0.05f;
@@ -169,28 +163,37 @@ public class Draw : MonoBehaviour {
         }
     }
 
-
+    /// <summary>
+    /// Generate a Normalized List of points, that are in camera space in a 1x1x1 cube
+    /// </summary>
+    /// <param name="originalPoints"><c>List<Vector3></c> with original point list (should be larger than the MAX_POINTS variable)</param>
+    /// <param name="transform">Transform of the parent. This is necessary to sutract position to move the points from global to local space</param>
+    /// <returns></returns>
     Vector3[] GenerateNormalizedList(List<Vector3> originalPoints, Transform transform) {
         List<Vector3> normalizedPoints = new List<Vector3>();
         normalizedPoints.Add(originalPoints[0] - transform.position); // Always add the first point
 
+        // calculate the length of the complete path
         float totalLength = 0f;
         for (int i = 1; i < originalPoints.Count; i++) {
             totalLength += Vector3.Distance(originalPoints[i], originalPoints[i - 1]);
         }
 
-        float segmentLength = totalLength / (20f);
-        float distanceToNextPoint = segmentLength;
+        // divide the length by MAX_POINTS get an estimated length for each segment (distance between the new points)
+        float segmentLength = totalLength / MAX_POINTS;
         Vector3 lastPoint = originalPoints[0];
         float distanceBetweenPoints = 0.0f;
 
-        for (int i = 1; i < originalPoints.Count - 1 && normalizedPoints.Count < 20; i++) {
+        // loop through the original points until MAX_POINTS-1 Points are in the list
+        for (int i = 1; i < originalPoints.Count - 1 && normalizedPoints.Count < MAX_POINTS; i++) {
             Vector3 currentPoint = originalPoints[i];
 
+            // add the distance between this point and the previous point to distanceBetweenPoints (-> distance between last added point and current point) 
             distanceBetweenPoints += Vector3.Distance(originalPoints[i - 1], originalPoints[i]);
 
-            if (distanceBetweenPoints >= distanceToNextPoint) {
-                Vector3 pointOnSegment = lastPoint + distanceToNextPoint * (currentPoint - lastPoint).normalized;
+            // if distanceBetweenPoints is greater than the segmentLength, add a normalized point and reset distanceBetweenPoints
+            if (distanceBetweenPoints >= segmentLength) {
+                Vector3 pointOnSegment = lastPoint + segmentLength * (currentPoint - lastPoint).normalized;
                 normalizedPoints.Add(pointOnSegment - transform.position);
                 
                 distanceBetweenPoints = 0.0f;
@@ -198,12 +201,15 @@ public class Draw : MonoBehaviour {
             }
         }
 
+        // add the last point to the list
         normalizedPoints.Add(originalPoints[originalPoints.Count - 1] - transform.position);
 
-        while (normalizedPoints.Count < 20) {
+        // this is just a fallback to make sure that there are always the correct number of points. This should never happen and if it happens the results are probably crappy
+        while (normalizedPoints.Count < MAX_POINTS) {
             normalizedPoints.Add(originalPoints[originalPoints.Count - 1] - transform.position);
         }
 
+        // translate the generated pointlist into camera space
         Vector3[] cameraPoints = new Vector3[normalizedPoints.Count];
         for (int i = 0; i < normalizedPoints.Count; i++) {
             cameraPoints[i] = Camera.main.WorldToScreenPoint(transform.TransformPoint(normalizedPoints[i]));
@@ -212,6 +218,12 @@ public class Draw : MonoBehaviour {
         return NormalizeList(cameraPoints);
     }
 
+    /// <summary>
+    /// Method <c>NormalizeList</c> takes a Vector3[] and normalizes it to a range between -1 and 1.
+    /// First the displacement of the positions are corrected and then the data is normalized to a 1x1x1 cube
+    /// </summary>
+    /// <param name="standarizedPositions">Vector3[]</param>
+    /// <returns></returns>
     private Vector3[] NormalizeList(Vector3[] standarizedPositions) {
         float smallestX = 10000;
         float smallestY = 10000;
@@ -249,19 +261,58 @@ public class Draw : MonoBehaviour {
         return standarizedPositions;
     }
 
-    /* This is a Helper Function for retrieving Actions/Usages. This is helpful because we don't know what
-     * device the player is using (oculus/vive/index...) */
-    void LogFeatures() {
+
+    /// <summary>
+    /// This is a helper functionb for retrieving and logging Actions/Usages of various VR Setups. This might be helpuful as we
+    /// don't know what device the player might use (oculus/vive/index...)
+    /// <param name="type"><c>string</c> optional parameter. Define which Actions/Usages should be logged. possible parameters: "bool", "byte[]", "uint", "float", "Vector2", "Vector3". Default: Log Everything</param>
+    /// </summary>
+    void LogFeatures(string type = "") {
         List<InputFeatureUsage> features = new List<InputFeatureUsage>();
         device.TryGetFeatureUsages(features);
 
-        // Loop for Logging Feature types
         foreach (var feature in features) {
-            //Debug.Log($"feature {feature.name} type {feature.type}");
 
-            // only log bool types (e.g. want to get trigger/button press)
-            if (feature.type == typeof(bool)) {
-                Debug.Log($"feature {feature.name} type {feature.type}");
+            switch(type) {
+                case "bool":
+                    if (feature.type == typeof(bool)) {
+                        Debug.Log($"feature {feature.name} type {feature.type}");
+                    }
+                    break;
+                    
+                case "byte[]":
+                    if (feature.type == typeof(byte[])) {
+                        Debug.Log($"feature {feature.name} type {feature.type}");
+                    }
+                    break;
+                    
+                case "uint":
+                    if (feature.type == typeof(uint)) {
+                        Debug.Log($"feature {feature.name} type {feature.type}");
+                    }
+                    break;
+                    
+                case "float":
+                    if (feature.type == typeof(float)) {
+                        Debug.Log($"feature {feature.name} type {feature.type}");
+                    }
+                    break;
+                    
+                case "Vector2":
+                    if (feature.type == typeof(Vector2)) {
+                        Debug.Log($"feature {feature.name} type {feature.type}");
+                    }
+                    break;
+                    
+                case "Vector3":
+                    if (feature.type == typeof(Vector3)) {
+                        Debug.Log($"feature {feature.name} type {feature.type}");
+                    }
+                    break;
+                    
+                default:
+                    Debug.Log($"feature {feature.name} type {feature.type}");
+                    break;
             }
         }
     }
--- README.md
+++ README.md
@@ -1,1 +1,14 @@
-# SpellSlinger_Prototyping
+# ~~SpellSlinger_Prototyping~~ That 1 Wizard Game
+Includes some basic setups and experiments for the upcoming VR Project "That 1 Wizard Game"
+## Saving Runes
+Open the *Draw_Prototype_01* scene and hit the play button. 
+
+On startup the necessary VR Software should automatically start (e.g. steamVR for index/vive).
+
+If you draw a rune, the rune is automatically saved to a .txt file. The file can be changed in the Draw.cs script (constant *FILEPATH* at the top of the file).
+
+Everytime a rune is drawn, several things happen:
+ - A new GameObject called "Original Rune X" with a LineRenderer is created. "X" is the number of Runes (e.g. if there are already 5 Runes in the .txt file it would be "Original Rune 6").
+ - A second GameObject called "Normalized Rune X" with a LineRenderer is created. This LineRenderer draws the rune as it was saved to the file. This can be helpful for debugging.
+
+ After you drew a couple of runes click the *Pause Button* (do not Stop the game only Pause!). With the game stopped switch to the scene tab and click on each "Normalized Rune X". If for example *Normalized Rune 9* looks off (e.g. tracking glitch), you can just remove the 9th line in the file
